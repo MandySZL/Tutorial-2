3de464597b929cc04a843c2a833614c1d2855c90
MandySZL <mandyszl0315@gmail.com>
2025-11-10 22:30:53 +0800

    Revert "Update MauiProgram.cs"
    This reverts commit b4fe79e91700118ffdc51bbb6aa59fc2b511a563.

--- Tutorial2/Tutorial2/MauiProgram.cs
+++ Tutorial2/Tutorial2/MauiProgram.cs
@@ -8,17 +8,24 @@ namespace Tutorial2
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-
             builder
                 .UseMauiApp<App>()
-                .UsePrism(prism =>
+                .UsePrism(static (prism) =>
                 {
+                    // RegisterTypes returns void, so you can't chain methods from it.
                     prism.RegisterTypes(container =>
                     {
+                        // ...
                         container.RegisterForNavigation<MainPage, MainPageViewModel>();
+                    }); // <-- End the RegisterTypes statement here.
+
+                    // Call OnAppStart on the 'prism' variable directly.
+                    prism.OnAppStart(app =>
+                    {
+                        app.NavigateAsync("MainPage");
                     });
                 });
-
+            // ...
 #if DEBUG
             builder.Logging.AddDebug();
 #endif
